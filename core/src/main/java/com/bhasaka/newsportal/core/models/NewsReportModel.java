ac0c04f2c2c8e73e88770bcdd903570c01b0783e
nikhil <nikhilborutkar@gmail.com>
2024-11-29 20:20:03 +0530

    New Classes added

---
+++ core/src/main/java/com/bhasaka/newsportal/core/models/NewsReportModel.java
@@ -0,0 +1,13 @@
+package com.bhasaka.newsportal.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = Resource.class)
+public class NewsReport {
+
+    @ValueMapValue
+    private String category;
+
+}
---
+++ core/src/main/java/com/bhasaka/newsportal/core/models/Path.java
@@ -0,0 +1,4 @@
+package com.bhasaka.newsportal.core.models;
+
+public class Path {
+}
